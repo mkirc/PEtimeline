201553ce00488103eb03aba471cbcded6a94b84a
mkirc <m.p.kirchner@gmx.de>
2024-08-12 16:56:28 +0200

    coarse line of events from 2018-2023

--- events.py
+++ events.py
@@ -1,14 +1,164 @@
 
 timelineItems = [
     {
-        "date"  : "1970-01-01 00:00:00.0001",
-        "title" : "first unix timestamp",
-        "text"  : "Marks the first millisecond of the unixen",
+        "date"  : "",
+        "title" : "2018",
+        "text"  : "<hr>",
     },
     {
-        "date"  : "1970-01-01 00:00:01.0000",
-        "title" : "second unix second",
-        "text"  : "Marks the first second of the unixen",
+        "date"  : "2018",
+        "title" : "Vortrag von Ernst Ulrich v. Weizsäcker",
+        "text"  : "",
     },
+    {
+        "date"  : "",
+        "title" : "2019",
+        "text"  : "<hr>",
+    },
+    {
+        "date"  : "2019",
+        "title" : "Erste Public Climate School",
+        "text"  : "",
+    },
+    {
+        "date"  : "2019",
+        "title" : "Kürzungen an der UzK bedrohen viele Fachbereiche",
+        "text"  : """Uniweite Bündnisse entstehen → FS kommt mit dem Global
+        South Studies Center (GSSC) in Kontakt.</br> \"<small>Ein zentrales
+        Anliegen des GSSC ist – in enger Kooperation mit Partner*innen aus den
+        Ländern des Globalen Südens –, wissenschaftlich fundierte,
+        interdisziplinäre Projekte zu fördern. Das GSSC versteht sich dabei
+        immer auch als Inkubator für innovative gemeinsame Forschungsvorhaben
+        und als unterstützendes akademisches Umfeld für den wissenschaftlichen
+        Nachwuchs.</small>\"""",
+    },
+    {
+        "date"  : "2019",
+        "title" : "Gutachten des Wissenschaftsrats",
+        "text"  : "",
+    },
+    {
+        "date"  : "2019",
+        "title" : "ZSL und OVM",
+        "text"  : """Statt Studienbeiträge von Studierenden aus
+        Nicht-EU-Ländern zu erheben, wird die Landesregierung die sogenannten
+        Qualitätsverbesserungsmittel (QVM) für die Hochschulen von bisher 249
+        Millionen Euro auf 300 Millionen Euro jährlich erhöhen. Ab 2021 sollen
+        weiterhin im Zuge des Zukunftsvertrags (ZSL) Studium und Lehre durch
+        900 Millionen Euro für NRW gestärkt werden.""",
+    },
+    {
+        "date"  : "",
+        "title" : "2020",
+        "text"  : "<hr>",
+    },
+    {
+        "date"  : "27. Januar 2020",
+        "title" : "Erster bestätigter Fall von Corona in Deutschland",
+        "text"  : ""
+    },
+    {
+        "date"  : "22. März 2020",
+        "title" : "Beginn erster Lockdown",
+        "text"  : ""
+    },
+    {
+        "date"  : "4. Mai 2020",
+        "title" : "Ende erster Lockdown",
+        "text"  : ""
+    },
+    {
+        "date"  : "Mai 2020",
+        "title" : "Friedens und Umweltphysik and der UzK?",
+        "text"  : """Im Kontext von einem Neustart von Physik und Ethik wird mit
+        Bezug auf das Gutachten des Wissenschaftsrats erstmals über die
+        Ansiedlung von einem Lehrauftrag für Friedens- und Umweltphysik an den
+        Physikalischen Instituten diskutiert. Das 1. Institut auf Grund der
+        möglichen  Anknüpfung an die Molekülphysik und das IKP auf Grund der
+        möglichen Anknüpfung an Nukleare Abrüstungsverifikation werden als
+        Kandidaten genannt."""
+    },
+    {
+        "date"  : "Oktober 2020",
+        "title" : "Gründung des Interdiscplinary Forum",
+        "text"  : """Mitglieder von Physik und Ethik und dem GSSC das
+        'Interdisciplinary Forum', als Plattform für interdiziplinären
+        Austausch und Startpunkt für interdisziplonäre Zentren im Abbild des
+        GSSC an der UzK""",
+    },
+    {
+        "date"  : "",
+        "title" : "2021",
+        "text"  : "<hr>",
+    },
+    {
+        "date"  : "Januar 2021",
+        "title" : "interdisciplinary.uni-koeln.de",
+        "text"  : """Das 'Interdisciplinary Forum' bekommt mit der Website
+        unter <a href='https://interdisciplinary.uni-koeln.de'
+        >interdisciplinary.uni-koeln.de</a> einen Internetauftritt.""",
+    },
+    {
+        "date"  : "6. Januar 2021",
+        "title" : "Beginn zweiter Corona-Lockdown",
+        "text"  : """""",
+    },
+    {
+        "date"  : "Sommersemester 2021",
+        "title" : "Ringvorlesung: 'Peace & Sustainability'",
+        "text"  : """Aus den Gesprächen über die Ansiedlung von Umwelt- und
+        Friendensphysik an der UzK entspringt die Idee, eine virtuelle
+        Ringvorlesung "Interdisciplinary Forum: Peace and Sustainability"
+        zu den übergreifenden Themen zu organisieren. 14
+        Speaker und Teilnehmende von Physik und Ethik geben Vorträge zu
+        Umweltforschung, Nuklearer Abrüstung und interdisziplinäerer
+        Zusammenarbeit. Die Vorlesungsreihe wird begleitet durch einen offenen
+        'Futurology' Workshop, in dem Zukunftsfragen diskutiert werden."""
+    },
+    {
+        "date"  : "Juli 2021",
+        "title" : "Die Restriktionen des 2. Corona-Lockdowns werden zurückgenommen",
+        "text"  : """""",
+    },
+    {
+        "date"  : "26. September 2021",
+        "title" : "Bundestagswahl 2021",
+        "text"  : """""",
+    },
+    {
+        "date"  : "10. Oktober 2021",
+        "title" : "Vollversammlung der FS Physik",
+        "text"  : """Auf der Vollversammlung der FS Physik werden zuerst die
+        Vorhaben zur Einrichtung eines 'Peace and Sustainability Studies
+        Center' (PSSC) und eines 'Ethics, Theory and History of Science Studies
+        Center' (ETHSSC) vorgestellt.""",
+    },
+    {
+        "date"  : "",
+        "title" : "2022",
+        "text"  : "<hr>",
+    },
+    {
+        "date"  : "22. Februar 2022",
+        "title" : "Russland beginnt den Angriffskrieg gegen die Ukraine",
+        "text"  : """""",
+    },
+    {
+        "date"  : "Sommersemester 2022",
+        "title" : "Ringvorlesung: 'Pollution'",
+        "text"  : """""",
+    },
+    {
+        "date"  : "",
+        "title" : "2023",
+        "text"  : "<hr>",
+    },
+    {
+        "date"  : "Sommersemester 2023",
+        "title" : "Seminarreihe: 'Quantum computing and beyond'",
+        "text"  : """""",
+    },
+
+
 ]
 
